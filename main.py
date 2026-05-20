c90a8527b671796b4f8466f60218e5a1cd1bd9e6
kodi nelson <kneldev@gmail.com>
2026-05-20 16:16:28 -0500

    user input accepted now, might be important right? project actually completed now.

--- main.py
+++ main.py
@@ -1,4 +1,14 @@
-from stats import list_the_dicts, word_count, char_count
+import sys
+from stats import CharacterCount, list_the_dicts, word_count, char_count
+
+
+def input_handler():
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    else:
+        book_path = sys.argv[1]
+        return book_path
 
 
 def get_book_text(path_to_file: str) -> str:
@@ -6,13 +16,14 @@ def get_book_text(path_to_file: str) -> str:
         return f.read()
 
 
-def format_output(sorted_counts):
+def format_output(sorted_counts: list[CharacterCount]) -> None:
     for item in sorted_counts:
         print(f"{item['char']}: {item['num']}")
 
 
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_path = input_handler()
+    book_text = get_book_text(book_path)
     num_words = word_count(book_text)
     character_counts = char_count(book_text)
     sorted_counts = list_the_dicts(character_counts)
